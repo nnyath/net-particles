bd3a764b5c61b45fe44aa76b5e401b2e8282b035
Kenny Inthirath <kenny.inthirath@gmail.com>
2017-10-05 19:32:59 -0700

    Rewrote to be more elegant

--- src/index.js
+++ src/index.js
@@ -1,153 +1,157 @@
 'use strict';
 
-let canvas = document.getElementById('board')
-let ctx = canvas.getContext ? canvas.getContext('2d') : undefined
 
-const win = window
+const
+  max_particles = 100,
+  velocity = {x: 1.5, y: 2},
+  color = 'white',
+  opacity = .7,
+  radius = 5,
+  offset = 30
 
-const DEFAULT_OPTS = {
-  MAX_POINTS : 30,
-  COLOR: 'white',
-  VELOCITY: 1.5,
-  OFFSET : 20,
-  OPACITY: 0.7,
-  SIZE: 5
-}
 
-class controller{
-  constructor(canvas,ctx){
-    this.ctx = ctx
+class Particle {
+
+  constructor(canvas, opts){
+    Object.assign(this,
+      { color, opacity, radius, offset },
+      { canvas }, 
+      {
+        x:Math.random() * canvas.width, 
+        y:Math.random() * canvas.height,
+        velocity : {x:(Math.random() - 0.5) * velocity.x, y:(Math.random() - 0.5) * velocity.y },
+        ctx: canvas.getContext('2d') ? canvas.getContext('2d') : undefined
+      }, 
+      opts)
   }
-}
 
-class particle{
+  update(){
+    if(this.x > this.canvas.width + this.offset || this.x < this.offset)
+      this.velocity.x = -this.velocity.x
 
-  constructor(canvas, opts = DEFAULT_OPTS, x = Math.random() * canvas.width, y = Math.random() * canvas.height, VELOCITY = { x : (Math.random() - 0.5) * opts.VELOCITY, y : (Math.random() - 0.5 * opts.VELOCITY) }){
-    this.parent = canvas
-    this.ctx = canvas.getContext ? canvas.getContext('2d') : undefined
-    this.opts = opts
-    this.x = x
-    this.y = y
-    this.VELOCITY = { x, y } = VELOCITY
+    if(this.y > this.canvas.height + this.offset || this.y < this.offset)
+      this.velocity.y = -this.velocity.y
+
+    Object.assign(this,
+      {
+        x : this.x+=this.velocity.x,
+        y : this.y+=this.velocity.y
+      })
   }
 
   draw(){
-    if(!this.ctx){
-      throw new error('Cannot draw on canvas')
+    try{
+      this.ctx.beginPath()
+
+      Object.assign(this.ctx, {
+          fillStyle : this.color, 
+          globalAlpha : this.opacity, 
+      })
+      
+      this.ctx.arc(this.x, this.y, this.radius,0,Math.PI*2)
+      this.ctx.fill()
+
+    }catch(e){
+      throw e
     }
-
-    this.ctx.beginPath()
-    this.ctx.fillStyle = this.opts.COLOR
-    this.ctx.globalAlpha = this.opts.OPACITY
-    this.ctx.arc(this.x, this.y, this.opts.SIZE, 0, Math.PI * 2)
-    this.ctx.fill()
   }
-
-  update(){
-    if(this.x > this.parent.width + this.opts.OFFSET || this.x < this.opts.OFFSET)
-      this.VELOCITY.x = -this.VELOCITY.x
-
-    if(this.y > this.parent.height + this.opts.OFFSET || this.y < this.opts.OFFSET)
-      this.VELOCITY.y = -this.VELOCITY.y
-
-    this.x += this.VELOCITY.x
-    this.y += this.VELOCITY.y  
-  }
-
 }
 
-class particleNetwork{
-  constructor(canvas, opts = DEFAULT_OPTS){
-    this.parent = canvas
-    this.ctx = canvas.getContext ? canvas.getContext('2d') : undefined
-    this.opts = opts
-    this.collection = new Array()
+class ParticleNetwork {
+
+  constructor(canvas, opts){
+    Object.assign(this,
+      { max_particles, velocity, color, opacity, radius, offset },
+      { canvas },
+      { 
+        particles : new Array(),
+        ctx : canvas.getContext('2d') ? canvas.getContext('2d') : undefined
+      },
+      opts
+    )
   }
 
   init(){
-    for(let i=0;i<this.opts.MAX_POINTS;i++){
-      this.collection.push(new particle(canvas, this.opts))
-      this.collection[i].draw()
+    for(let i = 0; i<this.max_particles; i++){
+      let opts = {color:this.color,opacity:this.opacity,radius:this.radius,offset:this.offset}
+      this.particles.push(new Particle(this.canvas, opts))
     }
   }
 
-  update(){
-    
-    let drawParticles = () => {
-      this.collection.forEach((particle,ind) => {
-        if(!this.ctx){
-          throw new error('Cannot draw on canvas')
-        }
-        particle.update() 
+  draw(){
+
+    const drawParticles = (cb) => {
+      let ind = 0
+      for(let particle of this.particles){
+        particle.update()
         particle.draw()
-        drawConnections(particle, ind)
-      })
+        if(typeof cb == 'function')
+          cb(this.particles,particle,ind)
+        ind++
+      }
     }
 
-    let drawConnections = (particle, ind) => {
-      for(let i=ind;this.collection.length>i;i++){
-        let distance = Math.sqrt( Math.pow(particle.x - this.collection[i].x, 2) + Math.pow(particle.y - this.collection[i].y, 2))
+    const drawConnections = (particles,particle,ind) => {
+      for(let i=ind; i<particles.length;i++){
+        let distance = Math.sqrt( Math.pow(particle.x - particles[i].x, 2) + Math.pow(particle.y - particles[i].y, 2))
         
         if(distance > 120)
           continue
 
         this.ctx.beginPath()
-        this.ctx.strokeStyle = this.opts.COLOR
-        this.ctx.globalAlpha = ((120 - distance) / 120) * this.opts.SIZE
-        this.ctx.lineWidth = (this.opts.SIZE * 2) / 7
+        Object.assign(this.ctx,{
+          strokeStyle : particle.color,
+          globalAlpha : (120 - distance) / 120,
+          lineWidth : particle.size / 7
+        })
         this.ctx.moveTo(particle.x, particle.y)
-        this.ctx.lineTo(this.collection[i].x, this.collection[i].y)
+        this.ctx.lineTo(particles[i].x, this.particles[i].y)
         this.ctx.stroke()
       }
     }
-    
-    drawParticles(this.collection)
-    // console.log('drawing')
-    
+
+    drawParticles(drawConnections)
   }
+
 }
 
-class networkController{
-  constructor(canvas, x = new Array(), ...y){
-    this.parent = canvas
-    this.ctx = canvas.getContext ? canvas.getContext('2d') : undefined
-    this.networks = Array.isArray(x) ? x : [x,...y]
+class NetParticle {
+  constructor(canvas, networks = [new ParticleNetwork(canvas)]){
+    Object.assign(this,
+      {
+        canvas,
+        ctx : canvas.getContext('2d') ? canvas.getContext('2d') : undefined, 
+        networks
+      })
   }
 
   init(){
-    this.networks.forEach( network => network.init()) 
+    for(let network of this.networks)
+      network.init()
   }
 
-  update(){
-    this.ctx.clearRect(0, 0, this.parent.width, this.parent.height);
-    this.networks.forEach( network => network.update()) 
-    win.requestAnimationFrame(this.update.bind(this))
+  play(){
+    this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height)
+    for(let network of this.networks)
+      network.draw()
+    window.requestAnimationFrame(this.play.bind(this))  
   }
-
 }
 
-let network = new particleNetwork(canvas)
-let network2 = new particleNetwork(canvas, {
-  MAX_POINTS : 15,
-  COLOR: 'white',
-  VELOCITY: 1,
-  OFFSET : 20,
-  OPACITY : .3,
-  SIZE : 7
-})
+let NP = new NetParticle(document.getElementById('board'))
+NP.init()
+NP.play()
 
-let network3 = new particleNetwork(canvas, {
-  MAX_POINTS : 15,
-  COLOR: 'white',
-  VELOCITY: 1,
-  OFFSET : 20,
-  OPACITY : .9,
-  SIZE : 3
-})
 
-console.log(network2.opts)
 
-let nc = new networkController(canvas, network, network2, network3)
-nc.init()
-nc.update()
 
+let resize = () => {
+  document.getElementById('board').width = window.innerWidth -100
+  document.getElementById('board').height = window.innerHeight-100
+}
+
+window.addEventListener('resize', resize, false)
+
+
+
+resize()
